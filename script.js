deac343c13d08b8dc0e408f22f670014286be98b
Leslie Claussen <leslie.claussen@mail.com>
2024-09-10 14:13:38 +0200

    best commit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const submitBtn = document.getElementById(submit - btn);
-const ratingComponent = document.getElementsByClassName(container);
-const thankYouComponent = document.getElementById();
+const submitBtn = document.getElementById("submit-btn");
+const ratingComponent = document.getElementById("rating-component");
+const thankYouComponent = document.getElementById("thank-you");
+const ratingButtons = document.querySelectorAll(".rating-btn");
+const ratingScore = document.getElementsByClassName("Dynamic-number");
+console.log(ratingScore);
+
+let defaultScore = 0;
+
+submitBtn.addEventListener("click", submitScore);
+
+function submitScore(e) {
+  e.preventDefault();
+  ratingComponent.style.display = "none";
+  thankYouComponent.style.display = "block";
+}
