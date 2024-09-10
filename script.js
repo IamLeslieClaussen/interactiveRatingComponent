c2b03d8455a1548ab6028d2dfdc3c76f2c6daea6
Leslie Claussen <leslie.claussen@mail.com>
2024-09-10 15:48:29 +0200

    latest commit

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const submitBtn = document.getElementById("submit-btn");
 const ratingComponent = document.getElementById("rating-component");
 const thankYouComponent = document.getElementById("thank-you");
 const ratingButtons = document.querySelectorAll(".rating-btn");
-const ratingScore = document.getElementsByClassName("Dynamic-number");
+const ratingScore = document.querySelector(".Dynamic-number");
 console.log(ratingScore);
 
-let defaultScore = 0;
+ratingScore.textContent = "";
 
 submitBtn.addEventListener("click", submitScore);
+
+ratingButtons.forEach((btn) => {
+  btn.addEventListener("click", ratingButtonClicked);
+});
 
 function submitScore(e) {
   e.preventDefault();
   ratingComponent.style.display = "none";
   thankYouComponent.style.display = "block";
+}
+
+function ratingButtonClicked(e) {
+  if (e.target.classList.contains("rating-btn")) {
+    e.target.classList.toggle("active");
+    ratingScore.textContent = e.target.textContent;
+  }
 }
